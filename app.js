0ad439613a6f118810626d199d9aa73f6b77df50
Victor <vlopezmoles@gmail.com>
2022-10-11 11:05:40 +0200

    update

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: "http://www.thenetninja.co.uk",
       showBooks: true,
-      title: "The Fina Empire",
-      author: "Brandom Sanderson",
-      age: 45,
-    };
+      books: [
+        {
+          title: "name of the wind",
+          author: "patrick rothfuss",
+          img: "assets/2.jpg",
+          isFav: true,
+        },
+        {
+          title: "the final empire",
+          author: "brandon sanderson",
+          img: "assets/1.jpg",
+          isFav: false,
+        },
+        {
+          title: "the way of kings",
+          author: "brandon sanderson",
+          img: "assets/3.jpg",
+          isFav: true,
+        }
+      ]
+    }
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks
     },
+    toggleFav(book) {
+      book.isFav = !book.isFav
+    }
   },
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => book.isFav)
+    }
+  }
 })
 
 app.mount("#app")
